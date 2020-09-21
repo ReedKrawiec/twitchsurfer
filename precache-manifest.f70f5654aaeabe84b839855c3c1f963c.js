3d1e17d63227f3f6d7a9cc75eed65a6719a7f210
ReedKrawiec <reedkrawiec@gmail.com>
2020-09-21 15:44:22 -0400

    gh-pages

---
+++ precache-manifest.f70f5654aaeabe84b839855c3c1f963c.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22f69eaea47bfdc14ebf79e99c11be97",
+    "url": "/twitchsurfer/index.html"
+  },
+  {
+    "revision": "8fcb142fdb4b7dcebe49",
+    "url": "/twitchsurfer/static/css/2.a0babc62.chunk.css"
+  },
+  {
+    "revision": "34a79f31beb13c0f9ef0",
+    "url": "/twitchsurfer/static/css/main.796b3d78.chunk.css"
+  },
+  {
+    "revision": "8fcb142fdb4b7dcebe49",
+    "url": "/twitchsurfer/static/js/2.1f94daa8.chunk.js"
+  },
+  {
+    "revision": "415ff45c06cab1ee06c1f3dc0f2d88ca",
+    "url": "/twitchsurfer/static/js/2.1f94daa8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "34a79f31beb13c0f9ef0",
+    "url": "/twitchsurfer/static/js/main.c28c0fd9.chunk.js"
+  },
+  {
+    "revision": "13d08becdc9b20040873",
+    "url": "/twitchsurfer/static/js/runtime-main.560c5695.js"
+  },
+  {
+    "revision": "58aef543c97bbaf6a9896e8484456d98",
+    "url": "/twitchsurfer/static/media/Roboto-Medium.58aef543.ttf"
+  },
+  {
+    "revision": "11eabca2251325cfc5589c9c6fb57b46",
+    "url": "/twitchsurfer/static/media/Roboto-Regular.11eabca2.ttf"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/twitchsurfer/static/media/logo.ee7cd8ed.svg"
+  }
+]);
